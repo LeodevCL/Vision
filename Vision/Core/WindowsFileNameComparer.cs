da602ccf74d731f3c4a05e8064404cb34c0fb4ed
LeodevCL <anteux@live.cl>
2020-07-23 02:07:03 -0400

    Algoritmo de ordenamiento nativo
    Ahora las imagenes se ordenarán igual como lo hace el Explorador de Windows, ya que los métodos habituales ordenan alfabéticamente pero no dan el mismo resultado.
    
    Más info en:
    https://es.stackoverflow.com/questions/375583/ordenar-una-lista-alfab%c3%a9ticamente-igual-que-windows

---
+++ Vision/Core/WindowsFileNameComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Vision
+{
+    public class WindowsFileNameComparer : IComparer<FileInfo>
+    {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+        static extern int StrCmpLogicalW(String x, String y);
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return StrCmpLogicalW(x.Name, y.Name);
+        }
+    }
+}
